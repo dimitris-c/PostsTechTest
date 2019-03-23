636d99e9bc2daefc6c89bb29221c9d067f4004d4
Dimitris Chatzieleftheriou <dimmdesign@me.com>
2019-03-23 11:35:41 +0000

    Initial Commit

---
+++ PostsTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostsTechTest
+//
+//  Created by Dimitris Chatzieleftheriou on 23/03/2019.
+//  Copyright © 2019 Decimal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
